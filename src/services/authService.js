7141cb9a30204882e9917dbaffe97fbe82e8eb40
Andrea Ivanova <aiadea00@gmail.com>
2023-03-12 00:34:53 +0200

    Services added
    Added authentication and CRUD services  functions

---
+++ src/services/authService.js
@@ -0,0 +1,54 @@
+const baseUrl = "https://softuni-custom-server2.herokuapp.com";
+
+export async function login(email, password) {
+  const res = await fetch(`${baseUrl}/users/login`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  });
+
+  let jsonResult = await res.json();
+  if (res.ok) {
+    return jsonResult;
+  } else {
+    throw jsonResult.message;
+  }
+}
+
+export async function register(email, password) {
+  const res = await fetch(`${baseUrl}/users/register`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  });
+
+  let jsonResult = await res.json();
+  if (res.ok) {
+    return jsonResult;
+  } else {
+    throw jsonResult.message;
+  }
+}
+
+export function logout(token) {
+  return fetch(`${baseUrl}/users/logout`, {
+    headers: {
+      "content-type": "application/json",
+      "X-Authorization": token,
+    },
+  });
+}
+
+export function getUser() {
+  let username = localStorage.getItem("username");
+
+  return username;
+}
+
+export function isAuthenticated() {
+  return Boolean(getUser());
+}
---
+++ src/services/destinationService.js
@@ -0,0 +1,38 @@
+const baseUrl = "https://softuni-custom-server2.herokuapp.com/data";
+
+export async function getAll() {
+  let res = await fetch(`${baseUrl}/destinations`);
+  let data = await res.json();
+  let result = Object.values(data);
+  return result;
+}
+
+export async function create(destinationData, token) {
+  let res = await fetch(`${baseUrl}/destinations`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      "X-Authorization": token,
+    },
+    body: JSON.stringify({ ...destinationData, likes: [] }),
+  });
+
+  let data = await res.json();
+
+  return data;
+}
+
+export async function getOne(destinationId) {
+  let res = await fetch(`${baseUrl}/destinations/${destinationId}`);
+  let data = await res.json();
+  return data;
+}
+
+export function deleteDestination(destinationId, token) {
+  return fetch(`${baseUrl}/destinations/${destinationId}`, {
+    method: "DELETE",
+    headers: {
+      "X-Authorization": token,
+    },
+  }).then((res) => res.json());
+}
